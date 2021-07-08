4fcb058497e630794f1605b00c0247f1d901a826
KingHabib <zach.habib16@gmail.com>
2021-07-07 21:58:50 -0700

    Refactored game state handler into a separate class

---
+++ CS-Jukebox/GameLogic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSGSI;
+using CSGSI.Nodes;
+
+namespace CS_Jukebox
+{
+    class GameLogic
+    {
+        private GameStateListener gsl;
+        private MusicState musicState = MusicState.None;
+
+        public GameLogic()
+        {
+            StartGameListener();
+        }
+
+        void StartGameListener()
+        {
+            gsl = new GameStateListener(3000);
+            gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
+
+            if (!gsl.Start())
+            {
+                Console.WriteLine("Game State Listener failed to start.");
+            }
+            else
+            {
+                Console.WriteLine("Listening...");
+            }
+        }
+
+        void OnNewGameState(GameState gs)
+        {
+            if (gs.Map.JSON.Equals("{}") && musicState != MusicState.Menu)
+            {
+                musicState = MusicState.Menu;
+                Console.WriteLine("Main Menu");
+            }
+
+            if (gs.Round.Phase == RoundPhase.FreezeTime && musicState != MusicState.FreezeTime)
+            {
+                musicState = MusicState.FreezeTime;
+                Console.WriteLine("FreezeTime Begun");
+            }
+
+            if (musicState == MusicState.Menu) return;
+
+            if (gs.Round.Phase == RoundPhase.Live && musicState != MusicState.Live && musicState != MusicState.BombPlanted)
+            {
+                musicState = MusicState.Live;
+                Console.WriteLine("Round Begun");
+            }
+
+            if (gs.Round.Phase == RoundPhase.Over && musicState != MusicState.Over)
+            {
+                musicState = MusicState.Over;
+                Console.WriteLine("Round over");
+            }
+
+            if (gs.Round.Bomb == BombState.Planted && musicState != MusicState.BombPlanted)
+            {
+                musicState = MusicState.BombPlanted;
+                Console.WriteLine("Bomb Planted");
+            }
+
+            //Todo: ignore music states until first freezetime. Reset this condition when reaching the main menu.
+        }
+    }
+
+    public enum MusicState
+    {
+        None,
+        Menu,
+        FreezeTime,
+        Live,
+        BombPlanted,
+        Over
+    }
+}
--- CS-Jukebox/MainForm.cs
+++ CS-Jukebox/MainForm.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using CSGSI;
-using CSGSI.Events;
 using CSGSI.Nodes;
 using System.Runtime.InteropServices;
 
@@ -21,9 +20,7 @@ namespace CS_Jukebox
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
 
-        private GameStateListener gsl;
-        private MusicState musicState = MusicState.None;
-        private bool playMusic = false;
+        private GameLogic logic;
         private Jukebox jukebox;
 
         public MainForm()
@@ -39,77 +36,19 @@ namespace CS_Jukebox
             //popup so that user can browse to it.
             if (Properties.GameDir == null)
             {
-                Form dirPopup = new GamePathForm(StartGameListener); //Pass method to start game listener
+                Form dirPopup = new GamePathForm(SetupGameListener); //Pass method to start game listener
                 dirPopup.Show(this);
             }
             else
             {
-                StartGameListener();
+                SetupGameListener();
             }
         }
 
-        void StartGameListener()
+        void SetupGameListener()
         {
             Properties.CreateConfig();
-
-            gsl = new GameStateListener(3000);
-            gsl.NewGameState += new NewGameStateHandler(OnNewGameState);
-
-            if (!gsl.Start())
-            {
-                Console.WriteLine("Game State Listener failed to start.");
-            }
-            else
-            {
-                Console.WriteLine("Listening...");
-            }
-        }
-
-        void OnNewGameState(GameState gs)
-        {
-            if (gs.Map.JSON.Equals("{}") && musicState != MusicState.Menu)
-            {
-                musicState = MusicState.Menu;
-                Console.WriteLine("Main Menu");
-            }
-
-            if (gs.Round.Phase == RoundPhase.FreezeTime && musicState != MusicState.FreezeTime)
-            {
-                musicState = MusicState.FreezeTime;
-                Console.WriteLine("FreezeTime Begun");
-            }
-
-            if (musicState == MusicState.Menu) return;
-
-            if (gs.Round.Phase == RoundPhase.Live && musicState != MusicState.Live && musicState != MusicState.BombPlanted)
-            {
-                musicState = MusicState.Live;
-                Console.WriteLine("Round Begun");
-            }
-
-            if (gs.Round.Phase == RoundPhase.Over && musicState != MusicState.Over)
-            {
-                musicState = MusicState.Over;
-                Console.WriteLine("Round over");
-            }
-
-            if (gs.Round.Bomb == BombState.Planted && musicState != MusicState.BombPlanted)
-            {
-                musicState = MusicState.BombPlanted;
-                Console.WriteLine("Bomb Planted");
-            }
-
-            //Todo: ignore music states until first freezetime. Reset this condition when reaching the main menu.
-        }
-
-        void OnRoundBegin()
-        {
-            Console.WriteLine("Round begun.");
-        }
-
-        void OnBombPlanted()
-        {
-            Console.WriteLine("Bomb has been planted.");
+            logic = new GameLogic();
         }
 
         private void browseBtn_Click(object sender, EventArgs e)
@@ -125,15 +64,5 @@ namespace CS_Jukebox
         {
             jukebox.SetVolume(trackBar1.Value * 10);
         }
-    }
-
-    public enum MusicState
-    {
-        None,
-        Menu,
-        FreezeTime,
-        Live,
-        BombPlanted,
-        Over
     }
 }
