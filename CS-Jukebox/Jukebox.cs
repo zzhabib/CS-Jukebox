5f8c644ad0946332bf1cac93a14ab9b4d3da0347
KingHabib <zach.habib16@gmail.com>
2021-07-15 20:28:49 -0700

    Started timer development

--- CS-Jukebox/Jukebox.cs
+++ CS-Jukebox/Jukebox.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Media;
+using System.Windows.Forms;
 using WMPLib;
 
 namespace CS_Jukebox
@@ -13,10 +14,15 @@ namespace CS_Jukebox
         private WindowsMediaPlayer player;
         private SongProfile currentSong;
 
+        private Timer songTimer;
+        private Timer fadeTimer;
+
         public Jukebox()
         {
             player = new WindowsMediaPlayer();
+            SetupTimers();
         }
+
         public void PlaySong(string path)
         {
             player.URL = path;
@@ -34,10 +40,27 @@ namespace CS_Jukebox
             player.settings.setMode("loop", loop);
         }
 
+        public void PlaySong(SongProfile song, bool loop, int duration)
+        {
+            float volume = ((float)Properties.MasterVolume / 100) * (float)song.Volume;
+            currentSong = song;
+
+            player.settings.volume = (int)volume;
+            player.URL = song.Path;
+            player.controls.play();
+            player.settings.setMode("loop", loop);
+        }
+
         public void UpdateVolume()
         {
             float volume = ((float)Properties.MasterVolume / 100) * (float)currentSong.Volume;
             player.settings.volume = (int)volume;
+        }
+
+        private void SetupTimers()
+        {
+            Timer songTimer = new Timer();
+            Timer fadeTimer = new Timer();
         }
     }
 }
